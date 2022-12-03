33bc1814a877ea826da8c56e8e4ac7b19e676f78
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-12-03 16:46:57 +0000

    Explore page

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,19 +1,19 @@
 // import { Link } from "react-router-dom";
-// import Slider from '../components/Slider'
+import Slider from "../components/Slider";
 import rentCategoryImage from "../assets/jpg/rentCategoryImage.jpg";
 import sellCategoryImage from "../assets/jpg/sellCategoryImage.jpg";
 
 function Explore() {
   return (
-    <div className="font-[system-ui m-[1rem] sm:m-[2rem]">
+    <div className="font-[system-ui] m-[1rem] sm:m-[2rem]">
       <header>
         <p className="text-3xl lg:text-5xl font-bold">Explore</p>
       </header>
 
       <main>
-        {/* <Slider /> */}
+        <Slider />
 
-        <p className="exploreCategoryHeading mt-[3rem] font-bold">Categories</p>
+        <p className="mt-[3rem] font-bold">Categories</p>
         <div className="md:mt-[1rem] mt-[0.8rem] sm:flex justify-between">
           <a href="/category/rent" className="w-[48%]">
             <img
@@ -21,7 +21,7 @@ function Explore() {
               alt="rent"
               className="w-full object-cover min-h-[115px] sm:h-[20vw] h-full rounded-2xl my-0 mx-auto"
             />
-            <p className="font-[500] mt-[0.5rem] mb-[2rem] text-left">
+            <p className="font-[500] mt-[0.5rem] mb-[1rem] text-left">
               Places for rent
             </p>
           </a>
