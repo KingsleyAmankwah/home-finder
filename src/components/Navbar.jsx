1b02b9bbacd2cf8ea94de47bb9df092067f256a2
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-12-03 04:14:44 +0000

    Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,21 +2,29 @@ import React from "react";
 import { ReactComponent as OfferIcon } from "../assets/svg/localOfferIcon.svg";
 import { ReactComponent as ExploreIcon } from "../assets/svg/exploreIcon.svg";
 import { ReactComponent as PersonIcon } from "../assets/svg/personOutlineIcon.svg";
+import { useNavigate } from "react-router-dom";
 
 function Navbar() {
+  const navigate = useNavigate();
   return (
-    <div className="fixed left-0 bottom-0 right-0 h-[80px] flex items-center z-50 bg-slate-300">
+    <div className="fixed left-0 bottom-0 right-0 h-[80px] flex items-center z-50 bg-slate-300 font-[system-ui]">
       <nav className="w-full mt-[0.75rem] overflow-y-hidden">
         <ul className="flex items-center justify-evenly">
-          <li className="flex flex-col items-center cursor-pointer">
+          <li
+            className="flex flex-col items-center cursor-pointer"
+            onClick={() => navigate("/")}
+          >
             <ExploreIcon />
             <p>Explore</p>
           </li>
-          <li className="flex flex-col items-center">
+          <li
+            className="flex flex-col items-center cursor-pointer"
+            onClick={() => navigate("/Offers")}
+          >
             <OfferIcon />
             <p>Offers</p>
           </li>
-          <li className="flex flex-col items-center">
+          <li className="flex flex-col items-center cursor-pointer">
             <PersonIcon />
             <p>Profile</p>
           </li>
