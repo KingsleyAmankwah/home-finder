9e619a84b680a203e999b478da3f52a9257d05e0
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-12-03 05:39:48 +0000

    Offer Page

--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -3,13 +3,13 @@ import ListingItem from "../components/ListingItem";
 
 function Offers() {
   return (
-    <div className="category mb-[10rem]">
+    <div className="m-[2rem] mb-[10rem] font-[system-ui]">
       <header>
         <p className="text-5xl font-bold">Offers</p>
       </header>
 
       <main>
-        <ul className="p-0">
+        <ul className="p-0 mt-[3rem]">
           <ListingItem />
         </ul>
       </main>
@@ -17,7 +17,9 @@ function Offers() {
       <br />
       <br />
 
-      <p className="loadMore">Load More</p>
+      <p className="opacity-75 py-[0.25rem] px-[0.5] cursor-pointer w-[8rem] my-0 mx-auto text-center rounded-2xl bg-[#000] text-white font-bold">
+        Load More
+      </p>
     </div>
   );
 }
