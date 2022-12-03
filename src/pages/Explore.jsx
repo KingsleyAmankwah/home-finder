da0aa976006e43c26484e388ae8a60daae352056
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-12-03 12:28:47 +0000

    Explore Page

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -5,31 +5,33 @@ import sellCategoryImage from "../assets/jpg/sellCategoryImage.jpg";
 
 function Explore() {
   return (
-    <div className="font-[system-ui] m-[2rem]">
+    <div className="font-[system-ui m-[1rem] sm:m-[2rem]">
       <header>
-        <p className="text-5xl font-bold">Explore</p>
+        <p className="text-3xl lg:text-5xl font-bold">Explore</p>
       </header>
 
       <main>
         {/* <Slider /> */}
 
         <p className="exploreCategoryHeading mt-[3rem] font-bold">Categories</p>
-        <div className="mt-[1rem] flex justify-between">
+        <div className="md:mt-[1rem] mt-[0.8rem] sm:flex justify-between">
           <a href="/category/rent" className="w-[48%]">
             <img
               src={rentCategoryImage}
               alt="rent"
-              className="w-full object-cover min-h-[115px] h-[20vw] rounded-3xl my-0 mx-auto"
+              className="w-full object-cover min-h-[115px] sm:h-[20vw] h-full rounded-2xl my-0 mx-auto"
             />
-            <p className="font-[500] text-left">Places for rent</p>
+            <p className="font-[500] mt-[0.5rem] mb-[2rem] text-left">
+              Places for rent
+            </p>
           </a>
           <a href="/category/sale" className="w-[48%]">
             <img
               src={sellCategoryImage}
               alt="sell"
-              className="w-full object-cover min-h-[115px] h-[20vw] rounded-3xl my-0 mx-auto"
+              className="w-full object-cover min-h-[115px] sm:h-[20vw] h-full rounded-2xl my-0 mx-auto"
             />
-            <p className="font-[500] text-left">Places for sale</p>
+            <p className="font-[500] mt-[0.5rem] text-left">Places for sale</p>
           </a>
         </div>
       </main>
