1881f8cfa12a77a9a0154ed2cfc51e17d610ec1e
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-12-04 17:29:44 +0000

    Auth status

--- src/hooks/useAuthStatus.jsx
+++ src/hooks/useAuthStatus.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useRef } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 function useAuthStatus() {
-  return <div>useAuthStatus</div>;
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [checkingStatus, setCheckingStatus] = useState(true);
+  const isMounted = useRef(true);
+
+  useEffect(() => {
+    if (isMounted) {
+      const auth = getAuth();
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          setLoggedIn(true);
+        }
+        setCheckingStatus(false);
+      });
+    }
+
+    return () => {
+      isMounted.current = false;
+    };
+  }, [isMounted]);
+
+  return { loggedIn, checkingStatus };
 }
 
 export default useAuthStatus;
