0aeb75d2e47636e96a956901685688c151addf8d
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-12-05 03:52:57 +0000

    Navbar Component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,29 +6,30 @@ import { useNavigate } from "react-router-dom";
 
 function Navbar() {
   const navigate = useNavigate();
+
   return (
     <div className="fixed left-0 bottom-0 right-0 h-[80px] flex items-center z-50 bg-white font-[system-ui]">
       <nav className="w-full mt-[0.75rem] overflow-y-hidden">
         <ul className="flex items-center justify-around lg:justify-evenly">
           <li
-            className="flex flex-col items-center cursor-pointer"
+            className="flex flex-col items-center cursor-pointer text-[#8f8f8f]"
             onClick={() => navigate("/")}
           >
-            <ExploreIcon />
+            <ExploreIcon fill={`#8f8f8f`} />
             <p>Explore</p>
           </li>
           <li
-            className="flex flex-col items-center cursor-pointer"
+            className="flex flex-col items-center cursor-pointer text-[#8f8f8f]"
             onClick={() => navigate("/Offers")}
           >
-            <OfferIcon />
+            <OfferIcon fill={`#8f8f8f`} />
             <p>Offers</p>
           </li>
           <li
-            className="flex flex-col items-center cursor-pointer"
+            className="flex flex-col items-center cursor-pointer text-[#8f8f8f]"
             onClick={() => navigate("/sign-in")}
           >
-            <PersonIcon />
+            <PersonIcon fill={`#8f8f8f`} />
             <p>Profile</p>
           </li>
         </ul>
