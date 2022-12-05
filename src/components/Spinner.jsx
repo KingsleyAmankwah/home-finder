5c091a7f0cc0e88434880ba50d2f4aadfe47fb72
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-12-05 12:07:42 +0000

    Spinner component

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -3,7 +3,7 @@ import RingLoader from "react-spinners/RingLoader";
 function Spinner() {
   return (
     <div>
-      <div className="flex flex-col w-full h-[30vh]  items-center justify-center">
+      <div className="flex flex-col w-full h-[50vh] bg-black opacity-75 items-center justify-center">
         <RingLoader className="mb-3" color={"#000"} size={50} />
         <h1 className="text-3xl font-bolder text-center">
           We are adding new ideas to your feed!
