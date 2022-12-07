261b6fecf517be422fcf0a41237203172bf6e7e7
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-12-07 02:41:27 +0000

    createListing Page

--- src/pages/createListing.jsx
+++ src/pages/createListing.jsx
@@ -4,7 +4,7 @@ import Spinner from "../components/Spinner";
 
 function CreateListing() {
   const [loading, setLoading] = useState(false);
-
+  const [geolocationEnabled, setGeolocationEnabled] = useState(true);
   const [formData, setFormData] = useState({
     type: "rent",
     name: "",
@@ -37,7 +37,7 @@ function CreateListing() {
     longitude,
   } = formData;
 
-  const onChange = (e) => {
+  const onMutate = (e) => {
     let boolean = null;
 
     if (e.target.value === "true") {
@@ -66,14 +66,14 @@ function CreateListing() {
   }
 
   return (
-    <div className="font-[system-ui] p-[1rem] sm:p-[2rem]">
+    <div className="font-[system-ui] mb-[10rem] p-[1rem] sm:p-[2rem] h-full w-full">
       <header>
         <p className="text-3xl lg:text-5xl font-bold">Create a Listing</p>
       </header>
 
-      <main className="mt-4">
+      <main className="mt-4 ">
         <form onSubmit={onSubmit}>
-          <label className="formLabel font-[600] mt-4 block">Sell / Rent</label>
+          <label className="font-[600] mt-4 block">Sell / Rent</label>
 
           <div className="flex">
             <button
@@ -81,7 +81,7 @@ function CreateListing() {
               className={type === "sale" ? "formButtonActive" : "formButton"}
               id="type"
               value="sale"
-              onClick={onChange}
+              onClick={onMutate}
             >
               Sell
             </button>
@@ -90,21 +90,23 @@ function CreateListing() {
               className={type === "rent" ? "formButtonActive" : "formButton"}
               id="type"
               value="rent"
-              onClick={onChange}
+              onClick={onMutate}
             >
               Rent
             </button>
           </div>
 
-          <label className="formLabel font-[600] mt-4 block">Name</label>
+          <label className="font-[600] mt-4 block">Name</label>
           <input
-            className="formInputName flex justify-center items-center text-center bg-white font-[600] py-[0.9rem] px-[3rem] rounded-2xl mt-[0.5rem] mr-[0.5rem]"
+            className="flex justify-center items-center text-center bg-white font-[600] py-[0.9rem] px-[3rem] rounded-2xl mt-[0.5rem] mr-[0.5rem]"
             type="text"
             id="name"
             value={name}
+            placeholder="Name here... "
             maxLength="32"
             minLength="10"
             required
+            onChange={onMutate}
           />
 
           <div className="formRooms flex">
@@ -117,20 +119,20 @@ function CreateListing() {
                 type="number"
                 id="bedrooms"
                 value={bedrooms}
+                onChange={onMutate}
                 min="1"
                 max="50"
                 required
               />
             </div>
             <div>
-              <label className="formLabel font-[600] mt-4 block">
-                Bathrooms
-              </label>
+              <label className="font-[600] mt-4 block">Bathrooms</label>
               <input
                 className={`flex justify-center items-center text-center bg-white font-[600] py-[0.9rem] px-[3rem] rounded-2xl mt-[0.5rem] mr-[0.5rem] `}
                 type="number"
                 id="bathrooms"
                 value={bathrooms}
+                onChange={onMutate}
                 min="1"
                 max="50"
                 required
@@ -138,18 +140,16 @@ function CreateListing() {
             </div>
           </div>
 
-          <label className="formLabel font-[600] mt-4 block">
-            Parking spot
-          </label>
-          <div className="formButtons flex">
+          <label className="font-[600] mt-4 block">Parking spot</label>
+          <div className=" flex">
             <button
               type="button"
               id="parking"
               value={true}
-              className={parking ? "formButtonActive" : "formButton"}
+              className={parking === true ? "formButtonActive" : "formButton"}
               min="1"
               max="50"
-              onChange={onChange}
+              onClick={onMutate}
             >
               Yes
             </button>
@@ -160,20 +160,20 @@ function CreateListing() {
                 !parking && parking !== null ? "formButtonActive" : "formButton"
               }
               value={false}
-              onChange={onChange}
+              onClick={onMutate}
             >
               No
             </button>
           </div>
 
-          <label className="formLabel font-[600] mt-4 block">Furnished</label>
+          <label className="font-[600] mt-4 block">Furnished</label>
           <div className="flex">
             <button
               type="button"
               className={furnished ? "formButtonActive" : "formButton"}
               id="furnished"
               value={true}
-              onChange={onChange}
+              onClick={onMutate}
             >
               Yes
             </button>
@@ -186,56 +186,58 @@ function CreateListing() {
               }
               id="furnished"
               value={false}
-              onChange={onChange}
+              onClick={onMutate}
             >
               No
             </button>
           </div>
 
-          <label className="formLabel font-[600] mt-4 block">Address</label>
+          <label className="font-[600] mt-4 block">Address</label>
           <textarea
             className={`flex justify-center items-center text-center bg-white font-[600] py-[0.9rem] px-[3rem] rounded-2xl mt-[0.5rem] mr-[0.5rem] `}
             type="text"
             id="address"
             value={address}
+            onChange={onMutate}
+            placeholder="Address here..."
             required
           />
-
-          <div className="formLatLng flex">
-            <div>
-              <label className="formLabel font-[600] mt-4 block">
-                Latitude
-              </label>
-              <input
-                className={`flex justify-center items-center text-center bg-white font-[600] py-[0.9rem] px-[3rem] rounded-2xl mt-[0.5rem] mr-[0.5rem] `}
-                type="number"
-                id="latitude"
-                value={latitude}
-                required
-              />
+          {!geolocationEnabled && (
+            <div className="formLatLng flex">
+              <div>
+                <label className="font-[600] mt-4 block">Latitude</label>
+                <input
+                  className={`flex justify-center items-center text-center bg-white font-[600] py-[0.9rem] px-[3rem] rounded-2xl mt-[0.5rem] mr-[0.5rem] `}
+                  type="number"
+                  id="latitude"
+                  value={latitude}
+                  required
+                  onChange={onMutate}
+                />
+              </div>
+              <div>
+                <label className="font-[600] mt-4 block">Longitude</label>
+                <input
+                  className={`flex justify-center items-center text-center bg-white font-[600] py-[0.9rem] px-[3rem] rounded-2xl mt-[0.5rem] mr-[0.5rem] `}
+                  type="number"
+                  id="longitude"
+                  value={longitude}
+                  onChange={onMutate}
+                  required
+                />
+              </div>
             </div>
-            <div>
-              <label className="formLabel font-[600] mt-4 block">
-                Longitude
-              </label>
-              <input
-                className={`flex justify-center items-center text-center bg-white font-[600] py-[0.9rem] px-[3rem] rounded-2xl mt-[0.5rem] mr-[0.5rem] `}
-                type="number"
-                id="longitude"
-                value={longitude}
-                required
-              />
-            </div>
-          </div>
+          )}
 
-          <label className="formLabel font-[600] mt-4 block">Offer</label>
-          <div className="formButtons flex">
+          <label className="font-[600] mt-4 block">Offer</label>
+          <div className="flex">
             <button
               type="button"
               className={offer ? "formButtonActive" : "formButton"}
               name="offer"
               id="offer"
               value={true}
+              onClick={onMutate}
             >
               Yes
             </button>
@@ -246,6 +248,7 @@ function CreateListing() {
               type="button"
               id="offer"
               value={false}
+              onClick={onMutate}
             >
               No
             </button>
@@ -258,40 +261,51 @@ function CreateListing() {
               type="number"
               id="regularPrice"
               value={regularPrice}
+              onChange={onMutate}
               min="50"
               max="750000000"
               required
             />
+
+            {type === "rent" && (
+              <p className="formPriceText font-[600] ml-[-1.5rem]">
+                &#8373; / Month
+              </p>
+            )}
           </div>
 
-          <>
-            <label className="font-[600] mt-4 block">Discounted Price</label>
-            <input
-              className={`flex justify-center items-center text-center bg-white font-[600] py-[0.9rem] px-[3rem] rounded-2xl mt-[0.5rem] mr-[0.5rem] `}
-              type="number"
-              id="discountedPrice"
-              value={discountedPrice}
-              min="50"
-              max="750000000"
-            />
-          </>
+          {offer && (
+            <>
+              <label className="font-[600] mt-4 block">Discounted Price</label>
+              <input
+                className={`flex justify-center items-center text-center bg-white font-[600] py-[0.9rem] px-[3rem] rounded-2xl mt-[0.5rem] mr-[0.5rem] `}
+                type="number"
+                id="discountedPrice"
+                value={discountedPrice}
+                onChange={onMutate}
+                min="50"
+                max="750000000"
+              />
+            </>
+          )}
 
           <label className="font-[600] mt-4 block">Images</label>
-          <p className="imagesInfo opacity-75 text-xs">
+          <p className="opacity-75 text-xs">
             The first image will be the cover (max 6).
           </p>
           <input
-            className="formInputFile w-full bg-[#00cc66] text-white font-[600] mr-4 rounded-2xl border-none py-2 px-3"
+            className="formInputFile w-full"
             type="file"
             id="images"
             max="6"
             accept=".jpg,.png,.jpeg"
+            onChange={onMutate}
             multiple
             required
           />
           <button
             type="submit"
-            className=" flex justify-center items-center mt-[2rem] bg-[#00cc66] text-white rounded-2xl py-[0.85rem] px-[2rem] w-[80%] text-sm createListingButton"
+            className=" flex justify-center items-center mt-[5rem] bg-[#00cc66] text-white rounded-2xl py-[0.85rem] px-[2rem] w-[80%] text-sm createListingButton"
           >
             Create Listing
           </button>
