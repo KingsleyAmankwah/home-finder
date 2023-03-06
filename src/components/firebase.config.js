ce527d269726b3568b766b02f58f0ca639455a5e
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-03-07 00:46:28 +0530

    firebase

--- src/components/firebase.config.js
+++ src/components/firebase.config.js
@@ -1,14 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDq4dAWkmERLB_Ryat7o5wudk9s85GN9wY",
-  authDomain: "home-finder-7ca0e.firebaseapp.com",
-  projectId: "home-finder-7ca0e",
-  storageBucket: "home-finder-7ca0e.appspot.com",
-  messagingSenderId: "569693466130",
-  appId: "1:569693466130:web:d6cd62ee1f8ecf4714b2cb",
+  apiKey: "AIzaSyDeRwMNO1--JKpIfZXpdc_UNK5edjnVlUU",
+  authDomain: "home-finder-f9654.firebaseapp.com",
+  projectId: "home-finder-f9654",
+  storageBucket: "home-finder-f9654.appspot.com",
+  messagingSenderId: "938814513363",
+  appId: "1:938814513363:web:8b79d941bb0a652eedf426",
 };
 
 // Initialize Firebase
