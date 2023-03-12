52015895d289752b8626f600a7b411f1adbf8df4
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-03-12 16:13:09 +0530

    No Internet functionalities Added

--- src/App.js
+++ src/App.js
@@ -14,41 +14,65 @@ import CreateListing from "./pages/CreateListing";
 // import EditListing from './pages/EditListing'
 // import Listing from './pages/Listing'
 import Contact from "./pages/Contact";
+import NoInternet from "./components/NoInternet";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [isOnline, setIsOnline] = useState(navigator.onLine);
+
+  useEffect(() => {
+    const handleNetworkChange = () => {
+      setIsOnline(navigator.onLine);
+    };
+
+    window.addEventListener("online", handleNetworkChange);
+    window.addEventListener("offline", handleNetworkChange);
+
+    return () => {
+      window.removeEventListener("online", handleNetworkChange);
+      window.removeEventListener("offline", handleNetworkChange);
+    };
+  }, []);
+
   return (
-    <div className="App sm:bg-[#f2f4f8] m-0 p-0 w-full h-full">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Explore />} />
-          <Route path="/offers" element={<Offers />} />
-          <Route path="/sign-in" element={<SignIn />} />
-          <Route path="/sign-up" element={<SignUp />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/profile" element={<PrivateRoute />}>
-            <Route path="/profile" element={<Profile />} />
-          </Route>
-          <Route path="/create-listing" element={<PrivateRoute />}>
-            <Route path="/create-listing" element={<CreateListing />} />
-          </Route>
+    <>
+      <div>{!isOnline && <NoInternet />}</div>
 
-          <Route path="/contact" element={<Contact />} />
+      {isOnline && (
+        <div className="App sm:bg-[#f2f4f8] m-0 p-0 w-full h-full">
+          <Router>
+            <Routes>
+              <Route path="/" element={<Explore />} />
+              <Route path="/offers" element={<Offers />} />
+              <Route path="/sign-in" element={<SignIn />} />
+              <Route path="/sign-up" element={<SignUp />} />
+              <Route path="/forgot-password" element={<ForgotPassword />} />
+              <Route path="/profile" element={<PrivateRoute />}>
+                <Route path="/profile" element={<Profile />} />
+              </Route>
+              <Route path="/create-listing" element={<PrivateRoute />}>
+                <Route path="/create-listing" element={<CreateListing />} />
+              </Route>
 
-          {/* 
+              <Route path="/contact" element={<Contact />} />
+
+              {/* 
           <Route path='/category/:categoryName' element={<Category />} />
           <Route path='/edit-listing/:listingId' element={<EditListing />} />
           <Route
-            path='/category/:categoryName/:listingId'
-            element={<Listing />}
+          path='/category/:categoryName/:listingId'
+          element={<Listing />}
           />
           
-          */}
-        </Routes>
-        <Navbar />
-      </Router>
+        */}
+            </Routes>
+            <Navbar />
+          </Router>
 
-      <ToastContainer />
-    </div>
+          <ToastContainer />
+        </div>
+      )}
+    </>
   );
 }
 
