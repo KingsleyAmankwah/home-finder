cf70416f121c852b576afbb1c29b8011b9836264
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-03-12 16:13:46 +0530

    No Internet Component

---
+++ src/components/NoInternet.jsx
@@ -0,0 +1,25 @@
+import emoji from "../images/emoji.png";
+
+const NoInternet = () => {
+  return (
+    <div
+      className={`bg-cover bg-center h-screen flex flex-col items-center justify-center`}
+    >
+      <div className="">
+        <img
+          src={emoji}
+          alt="no_Internet_Image"
+          className="object-contain object-center"
+        />
+      </div>
+      <h1 className="text-3xl text-black font-bold mb-4">
+        Oops! No internet connection.
+      </h1>
+      <p className="text-xl text-black">
+        Please check your network settings and try again.
+      </p>
+    </div>
+  );
+};
+
+export default NoInternet;
