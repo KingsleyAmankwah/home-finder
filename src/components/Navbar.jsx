00294e1ea3cc1d572a640744627cda9e74d0a2f9
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-03-19 15:35:33 +0530

    Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,39 +2,36 @@ import React from "react";
 import { ReactComponent as OfferIcon } from "../assets/svg/localOfferIcon.svg";
 import { ReactComponent as ExploreIcon } from "../assets/svg/exploreIcon.svg";
 import { ReactComponent as PersonIcon } from "../assets/svg/personOutlineIcon.svg";
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Navbar() {
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
 
   return (
     <div className="fixed left-0 bottom-0 right-0 h-[80px] flex items-center z-50 bg-white font-[system-ui]">
       <nav className="w-full mt-[0.75rem] overflow-y-hidden">
         <ul className="flex items-center justify-around lg:justify-evenly">
-          <a
+          <li
             className="flex flex-col items-center cursor-pointer text-[#8f8f8f]"
-            // onClick={() => navigate("/")}
-            href="/"
+            onClick={() => navigate("/")}
           >
             <ExploreIcon fill={`#8f8f8f`} />
             <p>Explore</p>
-          </a>
-          <a
+          </li>
+          <li
             className="flex flex-col items-center cursor-pointer text-[#8f8f8f]"
-            // onClick={() => navigate("/Offers")}
-            href="/offers"
+            onClick={() => navigate("/Offers")}
           >
             <OfferIcon fill={`#8f8f8f`} />
             <p>Offers</p>
-          </a>
-          <a
+          </li>
+          <li
             className="flex flex-col items-center cursor-pointer text-[#8f8f8f]"
-            // onClick={() => navigate("/profile")}
-            href="/profile"
+            onClick={() => navigate("/profile")}
           >
             <PersonIcon fill={`#8f8f8f`} />
             <p>Profile</p>
-          </a>
+          </li>
         </ul>
       </nav>
     </div>
