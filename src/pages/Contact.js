56513bba3f94e11e564f9ded9995bdc59b664903
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-03-24 19:43:03 +0530

    Contact Page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,32 @@
-import { useState } from "react";
+import { doc, getDoc } from "firebase/firestore";
+import { useState, useEffect } from "react";
+import { useParams, useSearchParams } from "react-router-dom";
+import { toast } from "react-toastify";
+import { db } from "../firebase-config";
 
 function Contact() {
   const [message, setMessage] = useState("");
+  const [landlord, setLandlord] = useState(null);
+  // eslint-disable-next-line
+  const [searchParams, setSearchParams] = useSearchParams();
+  const params = useParams();
 
   const onChange = (e) => setMessage(e.target.value);
+
+  useEffect(() => {
+    const getLandlord = async () => {
+      const docRef = doc(db, 'users', params.landlordId)
+      const docSnap = await getDoc(docRef)
+
+      if (docSnap.exists()) {
+        setLandlord(docSnap.data())
+      } else {
+        toast.error('Could not get landlord data')
+      }
+    }
+
+    getLandlord()
+  }, [params.landlordId])
 
   return (
     <div className="pageContainer font-[system-ui] p-[1rem] sm:p-[2rem] mb-[10rem">
@@ -15,7 +38,7 @@ function Contact() {
       <main>
         <div className="contactLandlord flex mt-8 items-center">
           <p className="landlordName font-[600] text-[1.2rem]">
-            Contact Kingsley Amankwah
+            Contact {landlord?.name}
           </p>
         </div>
 
@@ -34,7 +57,11 @@ function Contact() {
             ></textarea>
           </div>
 
-          <a href="#!">
+          <a
+            href={`mailto:${landlord.email}?Subject=${searchParams.get(
+              "listingName"
+            )}&body=${message}`}
+          >
             <button
               type="submit"
               className=" flex justify-center items-center mt-[5rem] bg-[#00cc66] text-white rounded-2xl py-[0.85rem] px-[2rem] w-[80%] text-sm createListingButton"
